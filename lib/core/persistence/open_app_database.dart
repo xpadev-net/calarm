d899a8a6d63378f84670ee73639dd3d99c3e9d19
xpadev <xpadev@gmail.com>
2026-07-08 22:14:21 +0900

    Bundle Android sqlite native library

--- lib/core/persistence/open_app_database.dart
+++ lib/core/persistence/open_app_database.dart
@@ -7,6 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 
 Future<QueryExecutor> openAppDatabase(String name) async {
   try {
@@ -28,6 +29,7 @@ Future<void> configureSqliteForPlatform() async {
 
   try {
     final temporaryDirectory = await getTemporaryDirectory();
+    await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
     sqlite3.tempDirectory = temporaryDirectory.path;
   } on MissingPluginException catch (error) {
     debugPrint('Could not configure sqlite temp directory: $error');
