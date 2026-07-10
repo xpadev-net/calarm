e98967947c0ed82f5a6ec4156445ae5be06121ad
xpadev <xpadev@gmail.com>
2026-07-10 17:11:46 +0900

    Add AlarmKit usage description

--- ios/RunnerTests/RunnerTests.swift
+++ ios/RunnerTests/RunnerTests.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Flutter
 import UIKit
 import XCTest
@@ -7,6 +8,14 @@ class RunnerTests: XCTestCase {
   func testExample() {
     // If you add code to the Runner application, consider adding tests here.
     // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+  }
+
+  func testAlarmKitUsageDescriptionIsConfigured() throws {
+    let usageDescription = try XCTUnwrap(
+      Bundle.main.object(forInfoDictionaryKey: "NSAlarmKitUsageDescription") as? String
+    )
+
+    XCTAssertFalse(usageDescription.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
   }
 
 }
